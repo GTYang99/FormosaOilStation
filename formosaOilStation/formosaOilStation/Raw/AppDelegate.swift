bbf6c783c9605daf37fa342dae55bf86354a296a
GTYang <gtq.yang@gmail.com>
2025-08-15 09:20:30 +0800

    fix: 修改 Google Places API Key 注入方式

--- formosaOilStation/formosaOilStation/Raw/AppDelegate.swift
+++ formosaOilStation/formosaOilStation/Raw/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        if let key = Bundle.main.object(forInfoDictionaryKey: "GooglePlacesAPIKey") as? String {
+        if let key = Bundle.main.object(forInfoDictionaryKey: "GADApplicationIdentifier") as? String {
             GMSPlacesClient.provideAPIKey(key)
         }
         MobileAds.shared.start(completionHandler: nil)
